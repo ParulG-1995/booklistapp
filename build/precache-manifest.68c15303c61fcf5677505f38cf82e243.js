d7c84a76c5c748ced51ee16abe69c4aa674294c1
Parul Gupta <parul.gupta@ksolves.com>
2020-03-05 16:24:31 +0530

    First change

---
+++ build/precache-manifest.68c15303c61fcf5677505f38cf82e243.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b40e494b02312ea46dc80cc55ae11e5",
+    "url": "/booklistapp/index.html"
+  },
+  {
+    "revision": "bcfbf7c5ecedc20ba4b0",
+    "url": "/booklistapp/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "44bf6463e6446762d5cb",
+    "url": "/booklistapp/static/css/main.c57b7a82.chunk.css"
+  },
+  {
+    "revision": "bcfbf7c5ecedc20ba4b0",
+    "url": "/booklistapp/static/js/2.b8b11a58.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/booklistapp/static/js/2.b8b11a58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44bf6463e6446762d5cb",
+    "url": "/booklistapp/static/js/main.6cb38364.chunk.js"
+  },
+  {
+    "revision": "b2e710b583cba40f7c6e",
+    "url": "/booklistapp/static/js/runtime-main.3a456dc4.js"
+  }
+]);
